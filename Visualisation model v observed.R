7a3c9a334cdbfff85e98a47b72fd8a7e9916c282
thomaszwagerman <s1426548@ed.ac.uk>
2018-03-10 16:10:22 +0000

    further visualisation done

--- Visualisation model v observed.R
+++ Visualisation model v observed.R
@@ -15,19 +15,32 @@ setwd("/home/thomasz/Desktop/toolik_timeseries/toolik_timeseries")
 
 observed <- read.csv('observed_hour.csv', header = TRUE, skip = 0, sep = ",")
 modeldefault <- read.csv('default_hour.csv', header = TRUE, skip = 0, sep = ",")
+green <- read.csv('green_hour.csv', header = TRUE, skip = 0, sep = ",")
+evergreen <- read.csv('evergreen_hour.csv', header = TRUE, skip = 0, sep = ",")
 
 #Checking import----
 head(observed)
 head(modeldefault)
+head(green)
+head(evergreen)
 
 #Changing factor to date class----
 observed$date_time <- as.Date(observed$date_time, format = "%Y-%m-%d")
 modeldefault$ts_hour <- as.Date(modeldefault$ts_hour, format = "%Y-%m-%d")
+green$ts_hour <- as.Date(green$ts_hour, format = "%Y-%m-%d")
+evergreen$ts_hour <- as.Date(evergreen$ts_hour, format = "%Y-%m-%d")
+
 class(observed$date_time)
 class(modeldefault$ts_hour)
+class(green$ts_hour)
+class(evergreen$ts_hour)
 
 #Colourpicker----
-c("#6495ED", "#66CD00")
+c("#6495ED", #blue
+  "#EE2C2C", #red
+  "#66CD00", #lightgreen
+  "#006400" #darkgreen
+  )
 
 #Making own theme----
 theme_toolik <- function(){
@@ -43,10 +56,10 @@ theme_toolik <- function(){
         plot.margin = unit(c(1,1,1,1), units= , "cm")) + #adding 1cm margin
     theme(legend.text = element_text(size=12, face="italic"), #setting font for a legend
           legend.title = element_blank(), #removing legend title, blank
-          legend.position=c(0.9, 0.2)) #setting position for legend, 0 is bottom left, 1 is top right.  
+          legend.position=c(0.2, 0.9)) #setting position for legend, 0 is bottom left, 1 is top right.  
 }
 
-#First visualisation - Air Temperature----
+#First visualisation - Air Temperature, model vs observation----
 ggplot() +
   geom_point(data = observed, aes(x = date_time, y = air_temp_3m, colour = "red")) +
   geom_point(data = modeldefault, aes(x = ts_hour, y = t, colour = "blue")) +
@@ -55,6 +68,55 @@ ggplot() +
   scale_colour_manual(values=c("#6495ED", "#66CD00"),
                       labels=c("Observed","Modelled"))+ #adding legend labels
   ylab("Air Temperature "*"in"~degree*C)+
+  xlab("Date (in 2015)")
+
+#All air temperature----
+ggplot() +
+  geom_line(data = observed, aes(x = date_time, y = air_temp_3m, colour = "red")) +
+  geom_line(data = modeldefault, aes(x = ts_hour, y = t, colour = "blue")) +
+  geom_line(data = green, aes(x = ts_hour, y = t, colour = "green")) +
+  geom_line(data = evergreen, aes(x = ts_hour, y = t, colour = "darkgreen")) +
+  theme_toolik()+
+  scale_fill_manual(values = c("#6495ED", "#EE2C2C", "#66CD00", "#006400"))+ #custom colours
+  scale_colour_manual(values=c("#6495ED", "#EE2C2C", "#66CD00", "#006400"),
+                      labels=c("Observed","Default", "Green", "Evergreen"))+ #adding legend labels
+  ylab("Air Temperature "*"in"~degree*C)+
+  xlab("Date (in 2015)")
+
+#Soil surface temp----
+ggplot() +
+  geom_line(data = modeldefault, aes(x = ts_hour, y = tslb.1., colour = "blue")) +
+  geom_line(data = green, aes(x = ts_hour, y = tslb.1., colour = "green")) +
+  geom_line(data = evergreen, aes(x = ts_hour, y = tslb.1., colour = "darkgreen")) +
+  theme_toolik()+
+  scale_fill_manual(values = c("#EE2C2C", "#66CD00", "#006400"))+ #custom colours
+  scale_colour_manual(values=c("#EE2C2C", "#66CD00", "#006400"),
+                      labels=c("Default", "Green", "Evergreen"))+ #adding legend labels
+  ylab("Soil Surface Temperature "*"in"~degree*C)+
+  xlab("Date (in 2015)")
+
+#Soil heat flux temp----
+ggplot() +
+  geom_line(data = modeldefault, aes(x = ts_hour, y = grdflx, colour = "blue")) +
+  geom_line(data = green, aes(x = ts_hour, y = grdflx, colour = "green")) +
+  geom_line(data = evergreen, aes(x = ts_hour, y = grdflx, colour = "darkgreen")) +
+  theme_toolik()+
+  scale_fill_manual(values = c("#EE2C2C", "#66CD00", "#006400"))+ #custom colours
+  scale_colour_manual(values=c("#EE2C2C", "#66CD00", "#006400"),
+                      labels=c("Default", "Green", "Evergreen"))+ #adding legend labels
+  ylab("Soil Heat Flux (W/M^2)")+
+  xlab("Date (in 2015)")
+
+#Surface Sensible Heat Flux----
+ggplot() +
+  geom_line(data = modeldefault, aes(x = ts_hour, y = hfx, colour = "blue")) +
+  geom_line(data = green, aes(x = ts_hour, y = hfx, colour = "green")) +
+  geom_line(data = evergreen, aes(x = ts_hour, y = hfx, colour = "darkgreen")) +
+  theme_toolik()+
+  scale_fill_manual(values = c("#EE2C2C", "#66CD00", "#006400"))+ #custom colours
+  scale_colour_manual(values=c("#EE2C2C", "#66CD00", "#006400"),
+                      labels=c("Default", "Green", "Evergreen"))+ #adding legend labels
+  ylab("Surface Sensible Heat Flux (W/M^2)")+
   xlab("Date (in 2015)")
 
 #Calculating RMSE----
@@ -76,3 +138,6 @@ error <- observed$air_temp_3m - modeldefault$t
 # Example of invocation of functions
 rmse(error)
 mae(error)
+
+names(observed)
+names(modeldefault)
--- merging.py
+++ merging.py
@@ -19,7 +19,8 @@ import datetime as dt
 plt.style.use('seaborn-deep')
 inpath = ''
 inpath2 = ''
-
+inpath3 = ''
+inpath4 = ''
 
 ####################
 infilename1 = inpath+'tooldefault.d03.TS'
@@ -33,12 +34,29 @@ infilename2 = inpath2+'1-hour_converted.csv'
 df2 = pd.read_csv(infilename2,skiprows=1, sep = ',', header = None,
                  names = ["date_time", "air_temp_3m", "lw_dn_avg", "lw_up_avg", "sw_dn_avg", "sw_up_avg"])
 
+infilename3 = inpath3+'toolgreen.d03.TS'
+df3 = pd.read_csv(infilename3,delim_whitespace=1,skiprows=1, header = None,
+                 names = ["id", "ts_hour", "id_tsloc", "ix", "iy", "t", "q",
+                          "u", "v", "psfc", "glw", "gsw", "hfx", "lh",
+                          "grdflx", "tsk", "tslb(1)", "rainc",
+                          "rainnc", "clw"])
+
+infilename4 = inpath4+'tooleverg.d03.TS'
+df4 = pd.read_csv(infilename4,delim_whitespace=1,skiprows=1, header = None,
+                 names = ["id", "ts_hour", "id_tsloc", "ix", "iy", "t", "q",
+                          "u", "v", "psfc", "glw", "gsw", "hfx", "lh",
+                          "grdflx", "tsk", "tslb(1)", "rainc",
+                          "rainnc", "clw"])
+
 #df2 = df2.loc['2015-04-21':'2015-05-19']
 
 # Add the decimal hour data to the base datetime
 # Need to get this from the namelist or wrfout files eventually.
 df1['ts_hour'] = pd.to_datetime(dt.datetime(2015,4,21)+pd.to_timedelta(df1['ts_hour'], unit='h'))
 df2 = df2.set_index(pd.DatetimeIndex(df2['date_time']))
+df3['ts_hour'] = pd.to_datetime(dt.datetime(2015,4,21)+pd.to_timedelta(df3['ts_hour'], unit='h'))
+df4['ts_hour'] = pd.to_datetime(dt.datetime(2015,4,21)+pd.to_timedelta(df4['ts_hour'], unit='h'))
+
 #df2['date'] = pd.to_datetime(dt.datetime(2015,4,21)+pd.to_timedelta(df2['date'], unit='h'))
 
 # Convert temperatures to Celcius
@@ -46,10 +64,25 @@ df1['t']= df1['t']-273.16
 df1['tslb(1)']= df1['tslb(1)']-273.16
 df1['tsk']= df1['tsk']-273.16
 
-df1 = df1.set_index(['ts_hour'])
-startTime1=pd.to_datetime(df1.index[0])   # returns a TimeStamp
-endTime1=pd.to_datetime(df1.index[-1])
+df3['t']= df3['t']-273.16
+df3['tslb(1)']= df3['tslb(1)']-273.16
+df3['tsk']= df3['tsk']-273.16
 
+df4['t']= df4['t']-273.16
+df4['tslb(1)']= df4['tslb(1)']-273.16
+df4['tsk']= df4['tsk']-273.16
+
+df1 = df1.set_index(['ts_hour'])
+startTime=pd.to_datetime(df1.index[0])   # returns a TimeStamp
+endTime=pd.to_datetime(df1.index[-1])
+
+df3 = df3.set_index(['ts_hour'])
+startTime=pd.to_datetime(df3.index[0])   # returns a TimeStamp
+endTime=pd.to_datetime(df3.index[-1])
+
+df4 = df4.set_index(['ts_hour'])
+startTime=pd.to_datetime(df4.index[0])   # returns a TimeStamp
+endTime=pd.to_datetime(df4.index[-1])
 #df2 = df2.set_index(['date_time'])
 #startTime2=pd.to_datetime(df2.index[0])   # returns a TimeStamp
 #endTime2=pd.to_datetime(df2.index[-1])
@@ -57,9 +90,13 @@ endTime1=pd.to_datetime(df1.index[-1])
 # Take the mean over 30 minutes ie 30T
 df1=df1.resample('1D').mean()
 df2=df2.resample('1D').mean()
+df3=df3.resample('1D').mean()
+df4=df4.resample('1D').mean()
 
 df1.to_csv('default_hour.csv')
 df2.to_csv('observed_hour.csv')
+df3.to_csv('green_hour.csv')
+df4.to_csv('evergreen_hour.csv')
 #frames = [df1, df2]
 #results = pd.concat(frames)
 #print results
